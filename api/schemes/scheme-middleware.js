df3b74fc84e22477d1709695dbdea03f15dc93fc
Sharon Sanchez <sharontsanchez@gmail.com>
2022-02-16 22:16:10 -0600

    validateScheme middleware is set up

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -37,7 +37,16 @@ try{
   }
 */
 const validateScheme = (req, res, next) => {
-
+  const {scheme_name} = req.body
+if(
+  scheme_name === undefined ||
+  typeof scheme_name !== 'string' ||
+  !scheme_name.trim()
+){
+  next({status: 400, message: "invalid scheme_name"})
+} else{
+  next()
+}
 }
 
 /*
